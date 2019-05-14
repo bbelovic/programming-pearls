a117b6df4af2245edf6866c965aa7a7f2b6424ad
bbelovic <bbelovic@gmail.com>
2019-05-14 21:31:36 +0200

    continuing with column 1

--- src/main/kotlin/net/bbelovic/programmingpearls/column1/Column1.kt
+++ src/main/kotlin/net/bbelovic/programmingpearls/column1/Column1.kt
@@ -2,10 +2,11 @@ package net.bbelovic.programmingpearls.column1
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.*
 
 fun readLines(path: Path): List<Int> =
         Files.readAllLines(path)
                 .map { i -> i.toInt() }
                 .toList()
 
-fun mapToBitMap
+val bitSet = BitSet()
