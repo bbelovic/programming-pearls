49d9441c647ef72a2aed3175ea95416c5930634d
bbelovic <bbelovic@gmail.com>
2019-05-15 22:46:29 +0200

    mapping from bitmap to sorted array

--- src/main/kotlin/net/bbelovic/programmingpearls/column1/Column1.kt
+++ src/main/kotlin/net/bbelovic/programmingpearls/column1/Column1.kt
@@ -3,12 +3,24 @@ package net.bbelovic.programmingpearls.column1
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.*
-
-fun readLines(path: Path): List<Int> =
-        Files.readAllLines(path)
-                .map { i -> i.toInt() }
-                .toList()
+import java.util.stream.Collectors
 
 val bitSet = BitSet()
 
 val toBitSet: (BitSet, Int) -> BitSet = { bs, x -> bs.set(x); bs}
+
+
+fun readLines(input: List<String>): BitSet {
+    return input.asSequence()
+            .map { i -> i.toInt() }
+            .fold(BitSet()) { bs: BitSet, x: Int -> bs.set(x); bs}
+}
+
+fun sorted(bitSet: BitSet): List<Int> {
+    return bitSet
+            .stream()
+            .boxed()
+            .sorted()
+            .collect(Collectors.toList<Int>())
+}
+
