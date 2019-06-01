1319cf479adf248ecb0e164cb1e09e853a63996d
bbelovic <bbelovic@gmail.com>
2019-06-01 11:52:58 +0200

    programming pearls column 2, dummy rotate left implementation

---
+++ src/main/kotlin/net/bbelovic/programmingpearls/column1/Column2.kt
@@ -0,0 +1,17 @@
+package net.bbelovic.programmingpearls.column1
+
+fun rotateLeft(arr: CharArray, moves: Int) {
+    for (j in 0 until moves) {
+        val c = arr[0]
+        for (i in 1 until arr.size) {
+            arr[i - 1] = arr[i]
+        }
+        arr[arr.size - 1] = c
+    }
+}
+
+fun main() {
+    val arr = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+    rotateLeft(arr, 1)
+    print(arr)
+}
