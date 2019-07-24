36ada5f75f67ef99cd0298f14f1145db2cf80cbe
bbelovic <bbelovic@gmail.com>
2019-07-24 19:46:57 +0200

    JMH avgt time benchmark

--- src/main/kotlin/net/bbelovic/programmingpearls/column2/benchmark/Column2Benchmark.kt
+++ src/main/kotlin/net/bbelovic/programmingpearls/column2/benchmark/Column2Benchmark.kt
@@ -5,20 +5,17 @@ import net.bbelovic.programmingpearls.column2.rotateLeft4
 import org.openjdk.jmh.annotations.*
 
 @State(value = Scope.Thread)
-@BenchmarkMode(value = [Mode.Throughput])
+@BenchmarkMode(value = [Mode.AverageTime])
 open class Column2Benchmark {
     @Benchmark
-    fun benchmarkColumn2_rotateLeft1(): Int {
-        val input = "abcdefgh".toCharArray()
-        rotateLeft(input, 3)
-        return 1
+    fun benchmarkColumn2_rotateLeft1() {
+        val input = "abcdefghijklmnoprstuvxyz1234567890abcdefghijklmnoprstuvxyz1234567890".toCharArray()
+        rotateLeft(input, 8)
     }
 
     @Benchmark
-    fun benchmarkColumn2_rotateLeft4(): Int {
-        val input = "abcdefgh".toCharArray()
-        rotateLeft4(input, 3)
-        return 1
-
+    fun benchmarkColumn2_rotateLeft4() {
+        val input = "abcdefghijklmnoprstuvxyz1234567890abcdefghijklmnoprstuvxyz1234567890".toCharArray()
+        rotateLeft4(input, 8)
     }
 }
