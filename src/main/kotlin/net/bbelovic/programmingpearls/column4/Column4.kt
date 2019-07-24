62968acbc220fc2052a388542f8a268be189f53e
bbelovic <bbelovic@gmail.com>
2019-07-24 21:30:47 +0200

    Column 4, finished binary search

--- src/main/kotlin/net/bbelovic/programmingpearls/column4/Column4.kt
+++ src/main/kotlin/net/bbelovic/programmingpearls/column4/Column4.kt
@@ -1,7 +1,15 @@
 package net.bbelovic.programmingpearls.column4
 
-import java.util.*
-
 fun bsearch(t: Int, arr: Array<Int>): Int {
-    return Arrays.binarySearch(arr, t)
+    var l = 0
+    var u = arr.size - 1
+    while (l <= u) {
+        val m = (l + u) / 2
+        when {
+            arr[m] == t -> return m
+            arr[m] < t -> l = m + 1
+            else -> u = m - 1
+        }
+    }
+    return -1
 }
