8832f507ea8ba18a43133946b8c4211dcf02c35a
bbelovic <bbelovic@gmail.com>
2019-07-28 21:21:50 +0200

    Column 4, improving tests, round 2

--- src/test/kotlin/net/bbelovic/programmingpearls/column4/Column4KtTest.kt
+++ src/test/kotlin/net/bbelovic/programmingpearls/column4/Column4KtTest.kt
@@ -8,16 +8,18 @@ import org.junit.jupiter.params.provider.MethodSource
 class Column4KtTest {
     @ParameterizedTest
     @MethodSource("testData")
-    fun testBSearch(arr: Array<Int>, expectedPos: Int) {
-        val actualPos = bsearch(8, arr)
+    fun testBSearch(arr: Array<Int>, searchedElement: Int, expectedPos: Int) {
+        val actualPos = bsearch(searchedElement, arr)
         assertEquals(expectedPos, actualPos)
     }
 
     companion object {
         @JvmStatic
         private fun testData() = listOf(
-                Arguments { arrayOf(Array(10) {x -> x+1}, 7)},
-                Arguments { arrayOf(Array(0) {x -> x+1}, -1)}
+                Arguments { arrayOf(Array(10) {x -> x+1}, 8,  7)},
+                Arguments { arrayOf(Array(2) {x -> x+1}, 2, 1)},
+                Arguments { arrayOf(Array(1) {x -> x+1}, 1, 0)},
+                Arguments { arrayOf(Array(0) {x -> x+1}, 8, -1)}
         )
     }
 }
