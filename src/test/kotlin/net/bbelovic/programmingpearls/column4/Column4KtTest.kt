c6dd0b673399070c1ef91b19cf917292de86d239
bbelovic <bbelovic@gmail.com>
2019-07-28 21:17:45 +0200

    Column 4, improving tests

--- src/test/kotlin/net/bbelovic/programmingpearls/column4/Column4KtTest.kt
+++ src/test/kotlin/net/bbelovic/programmingpearls/column4/Column4KtTest.kt
@@ -1,13 +1,23 @@
 package net.bbelovic.programmingpearls.column4
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
 class Column4KtTest {
-    @Test
-    fun testBSearch() {
-        val arr = Array(10) {x -> x+1}
-        val actual = bsearch(8, arr)
-        assertEquals(7, actual)
+    @ParameterizedTest
+    @MethodSource("testData")
+    fun testBSearch(arr: Array<Int>, expectedPos: Int) {
+        val actualPos = bsearch(8, arr)
+        assertEquals(expectedPos, actualPos)
+    }
+
+    companion object {
+        @JvmStatic
+        private fun testData() = listOf(
+                Arguments { arrayOf(Array(10) {x -> x+1}, 7)},
+                Arguments { arrayOf(Array(0) {x -> x+1}, -1)}
+        )
     }
 }
