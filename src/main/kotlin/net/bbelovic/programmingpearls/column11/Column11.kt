8666a36df3cd270d107cb7c87cf7ad86ff512b27
bbelovic <bbelovic@gmail.com>
2019-08-20 11:28:47 +0200

    column11 - fixing qsort function, round 2

--- src/main/kotlin/net/bbelovic/programmingpearls/column11/Column11.kt
+++ src/main/kotlin/net/bbelovic/programmingpearls/column11/Column11.kt
@@ -1,9 +1,10 @@
 package net.bbelovic.programmingpearls.column11
 
 fun qsort1(input: Array<Int>, l: Int, u: Int) {
+    if (l >= u) return
     val m = partition(input, l, u)
-    qsort1(input, 0, m - 1)
-    qsort1(input, m + 1, m - 1)
+    qsort1(input, l, m - 1)
+    qsort1(input, m + 1, u)
 }
 
 fun partition(input: Array<Int>, l: Int, u: Int): Int {
