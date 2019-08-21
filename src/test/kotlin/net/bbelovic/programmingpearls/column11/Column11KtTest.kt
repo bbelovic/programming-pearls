4184181b631caf3270bdb7cc7602e6b54340ca86
bbelovic <bbelovic@gmail.com>
2019-08-21 21:39:38 +0200

    column11 - improving tests for qsort

--- src/test/kotlin/net/bbelovic/programmingpearls/column11/Column11KtTest.kt
+++ src/test/kotlin/net/bbelovic/programmingpearls/column11/Column11KtTest.kt
@@ -9,7 +9,7 @@ import org.junit.jupiter.params.provider.MethodSource
 
 internal class Column11KtTest {
     @ParameterizedTest
-    @MethodSource("testData")
+    @MethodSource("testDataPartition")
     fun testPartition(input: Array<Int>, l: Int, u: Int, expectedArray: Array<Int>, expectedM: Int) {
         val actualM = partition(input, l, u)
         assertEquals(expectedM, actualM)
@@ -18,19 +18,23 @@ internal class Column11KtTest {
 
     companion object {
         @JvmStatic
-        private fun testData() = listOf(
+        private fun testDataPartition() = listOf(
                 Arguments { arrayOf(arrayOf(7, 2, 1, 9, 5, 3, 0), 0, 6, arrayOf(0, 2, 1, 5, 3, 7, 9), 5) },
                 Arguments { arrayOf(arrayOf(7, 2, 1, 9), 0, 3, arrayOf(1, 2, 7, 9), 2) },
                 Arguments { arrayOf(arrayOf(5, 3, 0), 0, 2, arrayOf(0, 3, 5), 2) }
         )
+
+        @JvmStatic
+        private fun testDataQSort() = listOf(
+                Arguments { arrayOf(arrayOf(7, 2, 1, 9, 5, 3, 0), arrayOf(0, 1, 2, 3, 5, 7, 9)) }
+        )
     }
 
-    @Test
-    fun testQSort1() {
-        val input = arrayOf(7, 2, 1, 9, 5, 3, 0)
-        val expected = arrayOf(0, 1, 2, 3, 5, 7, 9)
+    @ParameterizedTest
+    @MethodSource("testDataQSort")
+    fun testQSort1(input: Array<Int>, expected: Array<Int>) {
         qsort1(input, 0, input.size - 1)
         assertArrayEquals(expected, input)
-
     }
+
 }
